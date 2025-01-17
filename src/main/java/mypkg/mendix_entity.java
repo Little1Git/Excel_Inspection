eb41020110688577929ee43d25e1d4a04bce3492
AQY2SZH <fixed-term.yi.qian2@cn.bosch.com>
2025-01-17 15:44:40 +0800

    222

--- src/main/java/mypkg/mendix_entity.java
+++ src/main/java/mypkg/mendix_entity.java
@@ -2,30 +2,58 @@
 //
 //public class mendix_entity {
 //    public static void main(String[] args) {
-//        this.Parameter.setPackagingPN(key_value_to_print.get("Packaging PN"));
-//        this.Parameter.setDescription(key_value_to_print.get("Description"));
-//        this.Parameter.setMaterial(key_value_to_print.get("Material"));
-//        this.Parameter.setWeight(key_value_to_print.get("Weight"));
-//        this.Parameter.setFEFCOtype(key_value_to_print.get("FEFCO Type"));
-//        this.Parameter.setInnerDimensions(key_value_to_print.get("Inner Dimensions"));
-//        this.Parameter.setOutsideDimensions(key_value_to_print.get("Outside Dimensions"));
-//        this.Parameter.setECT(key_value_to_print.get("ECT"));
-//        this.Parameter.setBST(key_value_to_print.get("BST"));
-//        this.Parameter.setBCT(key_value_to_print.get("BCT"));
-//        this.Parameter.setView(key_value_to_print.get("View"));
-//        this.Parameter.setManufacturersJoint(key_value_to_print.get("Manufacturer's Joint"));
-//        this.Parameter.setTypeofJoining(key_value_to_print.get("Type of Joining"));
-//        this.Parameter.setPrinting(key_value_to_print.get("Printing"));
-//        this.Parameter.setSortandorFluteCombination(key_value_to_print.get("Sort and/or Flute Combination"));
-//        this.Parameter.setMaterialThickness(key_value_to_print.get("(Material Thickness)"));
-//        this.Parameter.setGluedMoistureResistant(key_value_to_print.get("Glued Moisture-Resistant"));
-//        this.Parameter.setPET(key_value_to_print.get("PET"));
-//        this.Parameter.setInd(key_value_to_print.get("Ind."));
-//        this.Parameter.setChange(key_value_to_print.get("Change"));
-//        this.Parameter.setYYYYMMDD(key_value_to_print.get("YYYYMMDD"));
-//        this.Parameter.setDrawn(key_value_to_print.get("Drawn"));
-//        this.Parameter.setChecked(key_value_to_print.get("Checked"));
-//        this.Parameter.setRelease(key_value_to_print.get("Release"));
-//        this.Parameter.setRespdept(key_value_to_print.get("Resp. dept."));
+//
+//		this.Parameter_String_value.setPackagingPN(key_value_to_print.get("Packaging PN"));
+//        this.Parameter_String_value.setDescription(key_value_to_print.get("Description"));
+//        this.Parameter_String_value.setMaterial(key_value_to_print.get("Material"));
+//        this.Parameter_String_value.setWeight(key_value_to_print.get("Weight"));
+//        this.Parameter_String_value.setFEFCOtype(key_value_to_print.get("FEFCO Type"));
+//        this.Parameter_String_value.setInnerDimensions(key_value_to_print.get("Inner Dimensions"));
+//        this.Parameter_String_value.setOutsideDimensions(key_value_to_print.get("Outside Dimensions"));
+//        this.Parameter_String_value.setECT(key_value_to_print.get("ECT"));
+//        this.Parameter_String_value.setBST(key_value_to_print.get("BST"));
+//        this.Parameter_String_value.setBCT(key_value_to_print.get("BCT"));
+//        this.Parameter_String_value.setView(key_value_to_print.get("View"));
+//        this.Parameter_String_value.setManufacturersJoint(key_value_to_print.get("Manufacturer's Joint"));
+//        this.Parameter_String_value.setTypeofJoining(key_value_to_print.get("Type of Joining"));
+//        this.Parameter_String_value.setPrinting(key_value_to_print.get("Printing"));
+//        this.Parameter_String_value.setSortandorFluteCombination(key_value_to_print.get("Sort and/or Flute Combination"));
+//        this.Parameter_String_value.setMaterialThickness(key_value_to_print.get("(Material Thickness)"));
+//        this.Parameter_String_value.setGluedMoistureResistant(key_value_to_print.get("Glued Moisture-Resistant"));
+//        this.Parameter_String_value.setPET(key_value_to_print.get("PET"));
+//        this.Parameter_String_value.setInd(key_value_to_print.get("Ind."));
+//        this.Parameter_String_value.setChange(key_value_to_print.get("Change"));
+//        this.Parameter_String_value.setYYYYMMDD(key_value_to_print.get("YYYYMMDD"));
+//        this.Parameter_String_value.setDrawn(key_value_to_print.get("Drawn"));
+//        this.Parameter_String_value.setChecked(key_value_to_print.get("Checked"));
+//        this.Parameter_String_value.setRelease(key_value_to_print.get("Release"));
+//        this.Parameter_String_value.setRespdept(key_value_to_print.get("Resp. dept."));
+
+
+//		this.Parameter_true_or_false.setPackagingPN(pass_or_failure.get("Packaging PN"));
+//        this.Parameter_true_or_false.setDescription(pass_or_failure.get("Description"));
+//        this.Parameter_true_or_false.setMaterial(pass_or_failure.get("Material"));
+//        this.Parameter_true_or_false.setWeight(pass_or_failure.get("Weight"));
+//        this.Parameter_true_or_false.setFEFCOtype(pass_or_failure.get("FEFCO Type"));
+//        this.Parameter_true_or_false.setInnerDimensions(pass_or_failure.get("Inner Dimensions"));
+//        this.Parameter_true_or_false.setOutsideDimensions(pass_or_failure.get("Outside Dimensions"));
+//        this.Parameter_true_or_false.setECT(pass_or_failure.get("ECT"));
+//        this.Parameter_true_or_false.setBST(pass_or_failure.get("BST"));
+//        this.Parameter_true_or_false.setBCT(pass_or_failure.get("BCT"));
+//        this.Parameter_true_or_false.setView(pass_or_failure.get("View"));
+//        this.Parameter_true_or_false.setManufacturersJoint(pass_or_failure.get("Manufacturer's Joint"));
+//        this.Parameter_true_or_false.setTypeofJoining(pass_or_failure.get("Type of Joining"));
+//        this.Parameter_true_or_false.setPrinting(pass_or_failure.get("Printing"));
+//        this.Parameter_true_or_false.setSortandorFluteCombination(pass_or_failure.get("Sort and/or Flute Combination"));
+//        this.Parameter_true_or_false.setMaterialThickness(pass_or_failure.get("(Material Thickness)"));
+//        this.Parameter_true_or_false.setGluedMoistureResistant(pass_or_failure.get("Glued Moisture-Resistant"));
+//        this.Parameter_true_or_false.setPET(pass_or_failure.get("PET"));
+//        this.Parameter_true_or_false.setInd(pass_or_failure.get("Ind."));
+//        this.Parameter_true_or_false.setChange(pass_or_failure.get("Change"));
+//        this.Parameter_true_or_false.setYYYYMMDD(pass_or_failure.get("YYYYMMDD"));
+//        this.Parameter_true_or_false.setDrawn(pass_or_failure.get("Drawn"));
+//        this.Parameter_true_or_false.setChecked(pass_or_failure.get("Checked"));
+//        this.Parameter_true_or_false.setRelease(pass_or_failure.get("Release"));
+//        this.Parameter_true_or_false.setRespdept(pass_or_failure.get("Resp. dept."));
 //    }
 //}
