73f9b35b46a8c02089a5d06665d00582d22d95a5
AQY2SZH <fixed-term.yi.qian2@cn.bosch.com>
2025-01-17 13:25:38 +0800

    222

--- src/main/java/mypkg/read_float_textbox.java
+++ src/main/java/mypkg/read_float_textbox.java
@@ -1,32 +1,32 @@
-package mypkg;
-
-import org.apache.poi.xssf.usermodel.*;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-
-public class read_float_textbox {
-    public static String readFloatingTextbox(String filePath) {
-
-        try (FileInputStream fis = new FileInputStream(filePath);
-             XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
-
-            XSSFSheet sheet = workbook.getSheetAt(0);
-            XSSFDrawing drawing = sheet.createDrawingPatriarch();
-
-            if (drawing != null) {
-                for (XSSFShape shape : drawing.getShapes()) {
-                    if (shape instanceof XSSFSimpleShape) {
-                        XSSFSimpleShape simpleShape = (XSSFSimpleShape) shape;
-                        String text = simpleShape.getText();
-                        System.out.println(text);
-
-                    }
-                }
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-        return "finish";
-    }
-}
+//package mypkg;
+//
+//import org.apache.poi.xssf.usermodel.*;
+//
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//
+//public class read_float_textbox {
+//    public static String readFloatingTextbox(String filePath) {
+//
+//        try (FileInputStream fis = new FileInputStream(filePath);
+//             XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
+//
+//            XSSFSheet sheet = workbook.getSheetAt(0);
+//            XSSFDrawing drawing = sheet.createDrawingPatriarch();
+//
+//            if (drawing != null) {
+//                for (XSSFShape shape : drawing.getShapes()) {
+//                    if (shape instanceof XSSFSimpleShape) {
+//                        XSSFSimpleShape simpleShape = (XSSFSimpleShape) shape;
+//                        String text = simpleShape.getText();
+//                        System.out.println(text);
+//
+//                    }
+//                }
+//            }
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
+//        return "finish";
+//    }
+//}
